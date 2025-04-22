16c59027643a7446922f64226ef2ffc38e990fc6
imad tori <emad7@outlook.co.il>
2025-04-22 11:33:37 +0300

    first exe commit

---
+++ python_basics.py
@@ -0,0 +1,13 @@
+
+sen_len = len(input("enter a sentence: "))
+print(f"the length of the sentence is {sen_len}")
+
+##################
+
+my_fav_color = "blake"
+my_age=26
+my_favorite_animal="cat"
+
+# the names start with letter 
+# descriptive names
+# not using camelCase
