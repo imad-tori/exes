b391ceee0ce253cb0bacf8e2b9e6cb82440f9823
imad tori <emad7@outlook.co.il>
2025-04-26 20:40:55 +0300

    third commit

---
+++ type_Function.py
@@ -0,0 +1,7 @@
+num = 10
+str = "Hello"
+bool = True
+
+print(f"The value of num is {num} and its type is {type(num)}")
+print(f"The value of str is {str} and its type is {type(str)}")
+print(f"The value of bool is {bool} and its type is {type(bool)}")
